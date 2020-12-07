3db7ce7817dc5bd17a2c56aa9ae193bcf8283b03
BiroKage <bironader123@gmail.com>
2020-12-07 04:29:59 +0200

    create nav graph with toolbar

---
+++ app/src/main/java/com/example/nyarticles/framework/features/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.nyarticles.framework.features
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.nyarticles.R
+import com.example.nyarticles.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding get() = _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main
+        )
+        initAppBarWithNav()
+    }
+
+    private fun initAppBarWithNav() {
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.appBar
+            .setupWithNavController(navController, appBarConfiguration)
+    }
+}
