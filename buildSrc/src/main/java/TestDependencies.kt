40c9ca435670ab67be51714a3940983094742a9f
BiroKage <bironader123@gmail.com>
2020-12-08 05:04:11 +0200

    add test depedencies

---
+++ buildSrc/src/main/java/TestDependencies.kt
@@ -0,0 +1,9 @@
+object TestDependencies {
+
+    const val MOCKITO = "org.mockito:mockito-inline:${Versions.MOCKITO}"
+    const val JUNIT = "junit:junit:${Versions.JUNIT}"
+    const val ANDROID_ARC_TEST = "androidx.arch.core:core-testing:${Versions.ARCH_CORE_TEST}"
+    const val COROUTINES_TEST =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.COROUTINES}"
+
+}
